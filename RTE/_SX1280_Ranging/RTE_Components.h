8bc7aeeb9546ccf262e37223453023ef9c3902ab
FenixNJUPT <56378019+FenixNJUPT@users.noreply.github.com>
2020-04-12 16:45:07 +0800

    Add files via upload

---
+++ RTE/_SX1280_Ranging/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'SX1280_Ranging' 
+ * Target:  'SX1280_Ranging' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
